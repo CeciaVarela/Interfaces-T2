1a49d5c443d0958dae213f2e72ee44a743a883e8
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2022-09-28 10:34:31 +0200

    ejercicio 6

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,5 +1,6 @@
 from fibonacci import funcion_fibonacci
 from fibonacci2 import funcion_fibonacci2
+import time
 print("Elige una opcion:")
 print("a- funcion_fibonacci")
 print("b- funcion_fibonacci2")
@@ -7,10 +8,19 @@ resultado = input ("Teclea a o b:  ")
 if resultado == "a":
 	n=int(input("Enter the value of 'n': "))
 	print("Fibonacci Sequence:")
-	for n in range(0, n):
-		print(funcion_fibonacci(n))
+	start_time = time.time()
+	print(funcion_fibonacci(n))
+	end_time = time.time()
+	elapsed_time = end_time - start_time
+	print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
+	
+	
 elif resultado == "b":
 	n2=int(input("Enter the value of 'n': "))
-	for n2 in range(0, 	n2):
-		print (funcion_fibonacci2(n2))
+	print("Fibonacci Sequence:")
+	start_time = time.time()
+	print(funcion_fibonacci2(n2))
+	end_time = time.time()
+	elapsed_time = end_time - start_time
+	print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 
