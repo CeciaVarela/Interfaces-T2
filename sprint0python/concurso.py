6af797d47513922b5bbbeba2b96fe7208493ff16
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2022-09-28 08:52:15 +0200

    ejercicios 1 y 2

---
+++ sprint0python/concurso.py
@@ -0,0 +1,112 @@
+print("Que dia es hoy?")
+print("a-26")
+print("b-27")
+print("c-28")
+puntuacion = 0
+resultado = input ("Teclea a,b o c")
+if resultado == "a":
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+elif resultado == "b" :
+	print("Respuesta correcta")
+	puntuacion = puntuacion + 10
+	print("Llevas " + str(puntuacion))
+else:
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+	
+
+print("Cuanto es 5 x 3?")
+print("a-15")
+print("b-12")
+print("c-4")
+resultado = input ("Teclea a,b o c")
+if resultado == "b":
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+elif resultado == "a" :
+	print("Respuesta correcta")
+	puntuacion = puntuacion + 10
+	print("Llevas " + str(puntuacion))
+else:
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+
+
+print("Cuanto es 5 x 6?")
+print("a-25")
+print("b-12")
+print("c-30")
+resultado = input ("Teclea a,b o c")
+if resultado == "b":
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+elif resultado == "c" :
+	print("Respuesta correcta")
+	puntuacion = puntuacion + 10
+	print("Llevas " + str(puntuacion))
+else:
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+
+
+print("Cuanto es 9 x 10?")
+print("a-80")
+print("b-88")
+print("c-90")
+resultado = input ("Teclea a,b o c")
+if resultado == "b":
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+elif resultado == "c" :
+	print("Respuesta correcta")
+	puntuacion = puntuacion + 10
+	print("Llevas " + str(puntuacion))
+else:
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+
+
+print("Cuanto es 8 x 4?")
+print("a-32")
+print("b-27")
+print("c-30")
+resultado = input ("Teclea a,b o c")
+if resultado == "b":
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+elif resultado == "a" :
+	print("Respuesta correcta")
+	puntuacion = puntuacion + 10
+	print("Llevas " + str(puntuacion))
+else:
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+
+print("Cuanto es 3 x 7?")
+print("a-28")
+print("b-21")
+print("c-14")
+resultado = input ("Teclea a,b o c")
+if resultado == "a":
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
+elif resultado == "b" :
+	print("Respuesta correcta")
+	puntuacion = puntuacion + 10
+	print("Llevas " + str(puntuacion))
+else:
+	print("Respuesta incorrecta")
+	puntuacion = puntuacion - 5
+	print("Llevas " + str(puntuacion))
