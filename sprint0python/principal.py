06b1dbfac120b7059a7b99a1aeffc05b33efcdc7
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2022-09-28 10:16:36 +0200

    ejercicio 5

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,11 +1,16 @@
-#from fibonacci import funcion_fibonacci
-#n=int(input("Enter the value of 'n': "))
-#print("Fibonacci Sequence:")
-#for n in range(0, n):
-#  print(funcion_fibonacci(n))
- 
+from fibonacci import funcion_fibonacci
 from fibonacci2 import funcion_fibonacci2
-n2=int(input("Enter the value of 'n': "))
-for n2 in range(0, 	n2):
-	print (funcion_fibonacci2(n2))
+print("Elige una opcion:")
+print("a- funcion_fibonacci")
+print("b- funcion_fibonacci2")
+resultado = input ("Teclea a o b:  ")
+if resultado == "a":
+	n=int(input("Enter the value of 'n': "))
+	print("Fibonacci Sequence:")
+	for n in range(0, n):
+		print(funcion_fibonacci(n))
+elif resultado == "b":
+	n2=int(input("Enter the value of 'n': "))
+	for n2 in range(0, 	n2):
+		print (funcion_fibonacci2(n2))
 
