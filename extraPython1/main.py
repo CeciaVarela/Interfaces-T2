64d8e6ab46d26a1caa8766cdd2c1b1ae486f12bc
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2022-10-10 16:27:06 +0200

    Ejercicios extra 2

--- extraPython1/main.py
+++ extraPython1/main.py
@@ -4,3 +4,11 @@ def mayor(a,b):
     if(b > a):
         return b
 print(mayor(12,6))
+def max_de_tres(a,b,c):
+    if((a > b) and (a > c)):
+        return a
+    if((b > a) and (b > c)):
+        return b
+    if((c > a) and (c > b)):
+        return c
+print(max_de_tres(16,6,2));
