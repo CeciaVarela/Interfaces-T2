b763c65c722a24fd02869db8046e279df9474f07
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2022-10-26 10:48:46 +0200

    Ejercicio 6

--- sprint2catalog/catalog/detail_window.py
+++ sprint2catalog/catalog/detail_window.py
@@ -10,9 +10,9 @@ class DetailWindow(Gtk.Window):
 		box.pack_start(imagen, True, True,0)
 		box.pack_start(description, True, True,0)
 		self.add(box)
-		"""" Lo ajusto en el centro al iniciarlo y le doy un tamaño y un borde"""
+		#Lo ajusto en el centro al iniciarlo y le doy un tamaño y un borde"""
 		self.set_position(Gtk.WindowPosition.CENTER)
-		self.set_default_size(500, 300)
+		self.set_default_size(400, 400)
 		self.set_border_width(15)
 
 	
--- sprint2catalog/catalog/window.py
+++ sprint2catalog/catalog/window.py
@@ -2,7 +2,9 @@ import gi
 from gi.repository import GdkPixbuf
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
-from cell import Cell 
+from cell import Cell
+from tkinter import *
+from tkinter import messagebox as MessageBox
 
 class MainWindow(Gtk.Window):
 	flowbox = Gtk.FlowBox()
@@ -13,6 +15,20 @@ class MainWindow(Gtk.Window):
 		self.set_border_width(15)
 		self.set_default_size(400,400)
 		
+		mb = Gtk.MenuBar()#Creamos un menú
+		filemenu = Gtk.Menu()
+		filem = Gtk.MenuItem("Ayuda")
+		filem.set_submenu(filemenu)
+		
+		about = Gtk.MenuItem("Acerca de")
+		about.connect("activate", self.show_something )
+		filemenu.append(about)
+
+		mb.append(filem)
+
+		vbox = Gtk.VBox(False, 2)
+		vbox.pack_start(mb, False, False, 0)
+
 		header = Gtk.HeaderBar(title="Animales")
 		header.set_subtitle("Catálogo 2022")
 		header.props.show_close_button = True
@@ -22,13 +38,18 @@ class MainWindow(Gtk.Window):
 		scrolled = Gtk.ScrolledWindow()
 		scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
 		scrolled.add(self.flowbox)
-		self.add(scrolled)
+		vbox.add(scrolled)
+		self.add(vbox)
 		
 		for item in data_source:
 			cell = Cell(item.get("name"), item.get("gtk_image"), item.get("description"))
 			self.flowbox.add(cell)
 
-		self.set_position(Gtk.WindowPosition.CENTER)"""" Lo ajusto en el centro al iniciarlo y le doy un tamaño y un borde"""
+		self.set_position(Gtk.WindowPosition.CENTER)# Lo ajusto en el centro al iniciarlo y le doy un tamaño y un borde
 		self.set_default_size(500, 300)
 		self.set_border_width(15)
-		
+	
+	def show_something(self,event):
+			MessageBox.showinfo("Mensaje","acerca del desarrollador")
+
+	
