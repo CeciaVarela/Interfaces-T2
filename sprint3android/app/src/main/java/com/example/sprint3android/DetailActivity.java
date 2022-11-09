8ad5f62355a69b7b226a6c594d611bf36ea52c5a
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2022-11-09 10:42:47 +0100

    Ejercicio 3

--- sprint3android/app/src/main/java/com/example/sprint3android/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/sprint3android/DetailActivity.java
@@ -1,14 +1,25 @@
 package com.example.sprint3android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
-        //CircleImageView civ = (CircleImageView) findViewById(R.id.image_camiseta);
+        CircleImageView civ = (CircleImageView) findViewById(R.id.image_camiseta);
+        TextView miTexto = (TextView) findViewById(R.id.camiseta);
+        miTexto.setText("Camiseta");
+        civ.setImageResource(R.drawable.camiseta);
+        TextView description = (TextView) findViewById(R.id.camiseta_description);
+        description.setText("Es una prenda de ropa interior de abrigo por lo general de mangas cortas, cuello redondo o en forma de V");
     }
 }
