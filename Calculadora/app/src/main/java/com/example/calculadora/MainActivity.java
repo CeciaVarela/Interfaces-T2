b470c99a835554ac5df659cc4bf4684589c21036
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2022-12-12 18:21:33 +0100

    Calculadora

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -80,7 +80,7 @@ private String input,Answer;
             String number[]=input.split("\\*");
             try{
                 double mul=Double.parseDouble(number[0])*Double.parseDouble(number[1]);
-                input+=mul;
+                input=mul+"";
             }
             catch (Exception e){
                 //Toggle error
@@ -91,23 +91,59 @@ private String input,Answer;
             String number[]=input.split("/");
             try{
                 double div=Double.parseDouble(number[0])/Double.parseDouble(number[1]);
-                input+=div;
+                input=div+"";
             }
             catch (Exception e){
                 //Toggle error
             }
 
         }
-        else if(input.split("^").length==2){
+        else if(input.split("\\^").length==2){
             String number[]=input.split("\\^");
             try{
                 double pow =Math.pow(Double.parseDouble(number[0]),Double.parseDouble(number[1]));
-                input+=pow;
+                input=pow+"";
             }
             catch (Exception e){
                 //Toggle error
             }
-
         }
+        else if(input.split("\\+").length==2){
+            String number[]=input.split("\\+");
+            try{
+                double sum=Double.parseDouble(number[0])+Double.parseDouble(number[1]);
+                input=sum+"";
+            }
+            catch (Exception e){
+                //Toggle error
+            }
+        }
+        else if(input.split("-").length>1){
+            String number[]=input.split("-");
+            if(number[0]=="" && number.length==2){
+                number[0]=0+"";
+            }
+            try{
+                double sub=0;
+                if(number.length==2) {
+                    sub = Double.parseDouble(number[0]) - Double.parseDouble(number[1]);
+                }
+                else if(number.length==3){
+                    sub = Double.parseDouble(number[1]) - Double.parseDouble(number[2]);
+                }
+                input=sub+"";
+            }
+            catch (Exception e){
+                //Toggle error
+            }
+        }
+        String n[]=input.split("\\.");
+        if(n.length>1){
+            if(n[1].equals("0")){
+                input=n[0];
+            }
+        }
+        Screen.setText(input);
+
     }
 }
