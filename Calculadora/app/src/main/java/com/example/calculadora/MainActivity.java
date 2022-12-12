fcc30019b585485e5da624fa91c74b2a982bf865
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2022-12-12 17:42:26 +0100

    Calculadora

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,13 +2,112 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView Screen;
+private Button AC,Power,Back,Div,Mul,Plus,Min,One,Two,Three,Four,Five,Six,Seven,Eight,Nine,Zero,Equal,Ans,Point;
+private String input,Answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Screen=findViewById(R.id.screen);
+        AC=findViewById(R.id.ac);
+        Power=findViewById(R.id.power);
+        Back=findViewById(R.id.back);
+        Div=findViewById(R.id.div);
+        Seven=findViewById(R.id.seven);
+        Eight=findViewById(R.id.eight);
+        Nine=findViewById(R.id.nine);
+        Mul=findViewById(R.id.mul);
+        Four=findViewById(R.id.four);
+        Five=findViewById(R.id.five);
+        Six=findViewById(R.id.six);
+        Min=findViewById(R.id.min);
+        One=findViewById(R.id.one);
+        Two=findViewById(R.id.two);
+        Three=findViewById(R.id.three);
+        Plus=findViewById(R.id.plus);
+        Zero=findViewById(R.id.zero);
+        Point=findViewById(R.id.point);
+        Equal=findViewById(R.id.equal);
+        Ans=findViewById(R.id.ans);
+    }
+    public void ButtonClick(View view){
+        Button button=(Button) view;
+        String data=button.getText().toString();
+        switch (data){
+            case "AC":
+                input="";
+                break;
+            case "Ans":
+                input+=Answer;
+                break;
+            case "x":
+                Solve();
+                input+="*";
+                break;
+            case "^":
+                Solve();
+                input+="^";
+                break;
+            case "=":
+                Solve();
+                Answer=input;
+                break;
+            case "—":
+                String newText=input.substring(0,input.length()-1);
+                input=newText;
+                break;
+            default:
+                if(input==null){
+                    input="";
+                }
+                if(data.equals("+") || data.equals("-") || data.equals(("/"))){
+                    Solve();
+                }
+                input+=data;
+        }
+        Screen.setText(input);
+    }
+    private void Solve(){
+        if(input.split("\\*").length==2){
+            String number[]=input.split("\\*");
+            try{
+                double mul=Double.parseDouble(number[0])*Double.parseDouble(number[1]);
+                input+=mul;
+            }
+            catch (Exception e){
+                //Toggle error
+            }
+
+        }
+        else if(input.split("/").length==2){
+            String number[]=input.split("/");
+            try{
+                double div=Double.parseDouble(number[0])/Double.parseDouble(number[1]);
+                input+=div;
+            }
+            catch (Exception e){
+                //Toggle error
+            }
+
+        }
+        else if(input.split("^").length==2){
+            String number[]=input.split("\\^");
+            try{
+                double pow =Math.pow(Double.parseDouble(number[0]),Double.parseDouble(number[1]));
+                input+=pow;
+            }
+            catch (Exception e){
+                //Toggle error
+            }
+
+        }
     }
 }
