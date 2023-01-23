e41ab0b5b3f545018c0a8db4e3af361d29d49792
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2023-01-23 16:34:46 +0100

    ELIMINARCOMMIR Revert "Revert "Para mirar""
    This reverts commit f139339145a9b6ad410cb5fa220579abc87a911b.

---
+++ app/src/main/java/com/afundacion/entrenadorpersonal/ExerciseData.java
@@ -0,0 +1,44 @@
+package com.afundacion.entrenadorpersonal;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ExerciseData {
+    private String name;
+    private int time;
+    private int calories;
+    private String date;
+    private String category;
+
+    public ExerciseData(String name, int time, int calories, String date, String category) {
+        this.name = name;
+        this.time = time;
+        this.calories = calories;
+        this.date = date;
+        this.category = category;
+    }
+
+    public ExerciseData(JSONObject json) {
+        try {
+            this.name = json.getString("name");
+            this.time = json.getInt("time");
+            this.calories = json.getInt("calories");
+            this.date = json.getString("date");
+            this.category = json.getString("category");
+        } catch (JSONException error) {
+            error.printStackTrace();
+        }
+
+
+    }
+
+    public String getName() { return name; }
+
+    public int getTime() { return time; }
+
+    public int getCalories() { return  calories; }
+
+    public String getDate() { return date; }
+
+    public String getCategory() { return category; }
+}
