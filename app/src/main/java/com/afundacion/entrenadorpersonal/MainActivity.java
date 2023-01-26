d58374516b83b516ae14a7aaeead95481eeae3e6
Martín García Ramos <mgarciara@fpcoruna.afundacion.org>
2023-01-26 22:10:49 +0100

    Ejercicio 6: Adaptación al MockAPI completada.

--- app/src/main/java/com/afundacion/entrenadorpersonal/MainActivity.java
+++ app/src/main/java/com/afundacion/entrenadorpersonal/MainActivity.java
@@ -6,6 +6,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,49 +32,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        [{"time":58,"calories":22,"name":"name 1","date":"17/01/2023","category":"category 1","id":"1"},
-        {"time":30,"calories":40,"name":"name 2","date":"04/01/2023","category":"category 2","id":"2"},
-        {"time":60,"calories":94,"name":"name 3","date":"02/01/2023","category":"category 3","id":"3"},
-        {"time":50,"calories":20,"name":"name 4","date":"20/12/2022","category":"category 4","id":"4"},
-        {"time":86,"calories":77,"name":"name 5","date":"12/12/2023","category":"category 5","id":"5"}]
-         */
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                "https://63be86bc585bedcb36af7637.mockapi.io/Users/1/Exercise",
+                null,
+                new Response.Listener<JSONArray>(){
+                    @Override
+                    public void onResponse(JSONArray response){
+
+                        for(int i = 0; i<response.length();i++){
+                            try{
+                                Ejercicio ejercicio1 = new Ejercicio();
+                                JSONObject ejercicio = response.getJSONObject(i);
+                                ejercicio1.setNombre(ejercicio.getString("name"));
+                                ejercicio1.setTime(ejercicio.getInt("time"));
+
+                                ejercicio1.setCalorias(ejercicio.getInt("calories"));
+                                ejercicios.add(ejercicio1);
+                            }catch(JSONException jsonE){
+                                jsonE.printStackTrace();
+                            }
+                        }
 
 
-        Ejercicio ejercicio1 = new Ejercicio();
-        Ejercicio ejercicio2 = new Ejercicio();
-        Ejercicio ejercicio3 = new Ejercicio();
-        Ejercicio ejercicio4 = new Ejercicio();
-        Ejercicio ejercicio5 = new Ejercicio();
+                        RecyclerView ecRv = findViewById(R.id.rv_mep);
+                        registerForContextMenu(ecRv);
+                        ecRv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                        ecRv.setAdapter(new MEPAdapter(ejercicios));
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue cola = Volley.newRequestQueue(this);
+        cola.add(request);
 
-        ejercicio1.setNombre("name 1");
-        ejercicio2.setNombre("name 2");
-        ejercicio3.setNombre("name 3");
-        ejercicio4.setNombre("name 4");
-        ejercicio5.setNombre("name 5");
-
-        ejercicio1.setTime(58);
-        ejercicio2.setTime(30);
-        ejercicio3.setTime(60);
-        ejercicio4.setTime(50);
-        ejercicio5.setTime(86);
-
-        ejercicio1.setCalorias(22);
-        ejercicio2.setCalorias(40);
-        ejercicio3.setCalorias(94);
-        ejercicio4.setCalorias(20);
-        ejercicio5.setCalorias(77);
-
-        ejercicios.add(ejercicio1);
-        ejercicios.add(ejercicio2);
-        ejercicios.add(ejercicio3);
-        ejercicios.add(ejercicio4);
-        ejercicios.add(ejercicio5);
-
-        RecyclerView ecRv = findViewById(R.id.rv_mep);
-        registerForContextMenu(ecRv);
-        ecRv.setLayoutManager(new LinearLayoutManager(this));
-        ecRv.setAdapter(new MEPAdapter(ejercicios));
     }
 
     @Override
