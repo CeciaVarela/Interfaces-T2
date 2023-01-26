3bd4348c69694b12f53dbd280a9c23295a1b92da
Martín García Ramos <mgarciara@fpcoruna.afundacion.org>
2023-01-26 21:17:16 +0100

    Ejercicio 5: Adaptación del gráfico a MockAPI completada.

--- app/src/main/java/com/afundacion/entrenadorpersonal/MainActivity.java
+++ app/src/main/java/com/afundacion/entrenadorpersonal/MainActivity.java
@@ -1,19 +1,41 @@
 package com.afundacion.entrenadorpersonal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
+import com.android.volley.toolbox.JsonArrayRequest;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        4
+    }
+    /*
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -50,5 +72,5 @@ public class MainActivity extends AppCompatActivity {
         graph.getLegendRenderer().setVisible(true);
 
 
-    }
+    }*/
 }
