53873c713599335f84ebc7ebc709b4bb80f0ad74
Martín García <mgarciara@fpcoruna.afundacion.org>
2023-01-30 23:59:35 +0100

    Pantalla 5: Codigo transcrito a fragments

---
+++ app/src/main/java/com/afundacion/entrenadorpersonal/GraphicFragment.java
@@ -0,0 +1,93 @@
+package com.afundacion.entrenadorpersonal;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GraphicFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GraphicFragment extends Fragment {
+
+    public GraphicFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static GraphicFragment newInstance() {
+        GraphicFragment fragment = new GraphicFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_graphic, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                "https://63be86bc585bedcb36af7637.mockapi.io/Users/1/Exercise",
+                null,
+                response -> {
+                    List<DataPoint> ejercicios = new ArrayList<>();
+                    for(int i = 0; i<response.length();i++){
+                        try{
+                            JSONObject ejercicio = response.getJSONObject(i);
+                            DataPoint data = new DataPoint(i, ejercicio.getInt("calories"));
+                            ejercicios.add(data);
+                        }catch(JSONException jsonE){
+                            jsonE.printStackTrace();
+                        }
+                    }
+                    LineGraphSeries<DataPoint> seriePendiente = new LineGraphSeries<>(ejercicios.toArray(new DataPoint[0]));
+                    graph.addSeries(seriePendiente);
+                    graph.getLegendRenderer().setVisible(true);
+
+                },
+                error -> Toast.makeText(requireContext(), "Error", Toast.LENGTH_SHORT).show());
+        RequestQueue cola = Volley.newRequestQueue(requireContext());
+        cola.add(request);
+    }
+}
