55c99bf4db874ae9f2084a969ce1f55313df5fc5
lsilvag22 <113419011+lsilvag22@users.noreply.github.com>
2023-01-31 09:37:47 +0100

    Delete sprint1gtk directory

--- sprint1gtk/dialog/yes_window.py
+++
@@ -1,9 +0,0 @@
-import gi
-gi.require_version("Gtk","3.0")
-from gi.repository import Gtk
-
-class Yes_Window(Gtk.Window):
-	label = Gtk.Label("Tu respuesta es si")
-	def __init__(self):
-		super().__init__(title="Main")
-		self.add(self.label)
