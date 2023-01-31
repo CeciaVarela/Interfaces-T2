d51342aa7b6e0b73908cecb8336560b79a241a91
lsilvag22 <113419011+lsilvag22@users.noreply.github.com>
2023-01-31 09:37:25 +0100

    Delete sprint0python directory

--- sprint0python/fibonacci.py
+++
@@ -1,8 +0,0 @@
-def funcion_fibonacci(Number):
-    if(Number==0):
-        return 0
-    elif(Number==1):
-        return 1
-    else:
-        return (funcion_fibonacci(Number-2)+funcion_fibonacci(Number-1))
-
