261c7ff89e94285d0169ac734e686169fce1e57e
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2023-02-01 09:18:48 +0100

    Pantalla de  inicio

--- app/src/main/java/com/afundacion/entrenadorpersonal/HomeFragment.java
+++ app/src/main/java/com/afundacion/entrenadorpersonal/HomeFragment.java
@@ -146,11 +146,4 @@ public class HomeFragment extends Fragment {
         RequestQueue cola = Volley.newRequestQueue(context);
         cola.add(request);
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
 }
