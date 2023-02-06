60e46775042046b6a5a53d338885a43501b7c904
lsilvag22 <lsilvag22@fpcoruna.afundacion.org>
2023-02-06 16:31:54 +0100

    para mirar

--- app/src/main/java/com/afundacion/entrenadorpersonal/AddExercise.java
+++ app/src/main/java/com/afundacion/entrenadorpersonal/AddExercise.java
@@ -8,6 +8,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +21,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -32,13 +38,20 @@ public class AddExercise extends AppCompatActivity {
     private final Context context = null;
     public void onCreate(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        TextView timeTextViewCount = root.findViewById(R.id.text_view_timeCount);
-        TextView caloryTextViewCount = root.findViewById(R.id.text_view_caloryCount);
-        TextView idTextViewCount = root.findViewById(R.id.text_view_idCount);
-        final int[] timeTotal = {0};
-        final int[] caloriasTotales = {0};
-        final int[] idsTotales = {0};
+        View root = inflater.inflate(R.layout.activity_add_exercise, container, false);
+        EditText nameEditText = root.findViewById(R.id.exerciseName);
+        EditText dateEditText = root.findViewById(R.id.exerciseDate);
+        EditText durationEditText = root.findViewById(R.id.exerciseDuration);
+        EditText caloriesEditText = root.findViewById(R.id.exerciseCalories);
+        Spinner spinner = root.findViewById(R.id.category_spinner);
+        Button button = (Button) findViewById(R.id.button_send);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+        ArrayAdapter<CharSequence>adapter= ArrayAdapter.createFromResource(this, R.array.exercises, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
         SharedPreferences prefs = getSharedPreferences("Users", Context.MODE_PRIVATE);
         //String retrivedToken  = "token 1";
@@ -51,54 +64,44 @@ public class AddExercise extends AppCompatActivity {
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
+                        JSONObject exercise = null;
                         try {
-                            JSONObject exercise = response.getJSONObject(0);
-                            UsersData data = new UsersData(exercise);
-                            int idUsuario = data.getId();
-                            JsonArrayRequest request2 = new JsonArrayRequest(
-                                    Request.Method.POST,
-                                    "https://63be86bc585bedcb36af7637.mockapi.io/Users/"+String.valueOf(idUsuario)+"/Exercise",
-                                    null,
-                                    new Response.Listener<JSONArray>() {
-                                        @Override
-                                        public void onResponse(JSONArray response) {
-                                            DateFormat df = new SimpleDateFormat("dd/MM/yy");
-                                            for (int i = 0; i < response.length(); i++) {
-                                                try {
-                                                    JSONObject exercise = response.getJSONObject(i);
-                                                    ExerciseData data = new ExerciseData(exercise);
-                                                    Date bd = null;
-                                                    try {
-                                                        bd = df.parse(String.valueOf(data.getDate()));
-                                                        long bdDate = System.currentTimeMillis();
-                                                        if ((bdDate-bd.getTime()<=604800000)){
-                                                            caloriasTotales[0] += data.getCalories();
-                                                            timeTotal[0] += data.getTime();
-                                                            idsTotales[0] += 1;
-                                                        }
-                                                    } catch (ParseException e) {
-                                                        e.printStackTrace();
-                                                    }
-                                                } catch (JSONException e) {
-                                                    e.printStackTrace();
-                                                }
-                                            }
-                                            caloryTextViewCount.setText(String.valueOf(caloriasTotales[0]));
-                                            timeTextViewCount.setText(String.valueOf(timeTotal[0]));
-                                            idTextViewCount.setText(String.valueOf(idsTotales[0]));
-                                        }
-                                    },
-                                    new Response.ErrorListener() {
-                                        @Override
-                                        public void onErrorResponse(VolleyError error) {
-                                            Toast.makeText(context,"Error", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                            RequestQueue cola2 = Volley.newRequestQueue(context);
-                            cola2.add(request2);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                            exercise = response.getJSONObject(0);
+                        } catch (JSONException ex) {
+                            ex.printStackTrace();
                         }
+                        UsersData data = new UsersData(exercise);
+                        int idUsuario = data.getId();
+                        JSONObject requestBody = new JSONObject();
+                        try {
+                            requestBody.put("name", nameEditText.getText().toString());
+                            requestBody.put("date", dateEditText.getText().toString());
+                            requestBody.put("time", durationEditText.getText().toString());
+                            requestBody.put("calories", caloriesEditText.getText().toString());
+                            requestBody.put("category", spinner.toString());
+                            requestBody.put("UserId", String.valueOf(idUsuario));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                        JsonObjectRequest request2 = new JsonObjectRequest(
+                                Request.Method.POST,
+                                "https://63be86bc585bedcb36af7637.mockapi.io/Users/"+String.valueOf(idUsuario)+"/Exercise",
+                                requestBody,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        Toast.makeText(context, "Ejercicio añadido" , Toast.LENGTH_LONG).show();
+                                        finish();
+                                    }
+                                },
+                                new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Toast.makeText(context,"Error", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                        RequestQueue cola2 = Volley.newRequestQueue(context);
+                        cola2.add(request2);
                     }
 
                 },
